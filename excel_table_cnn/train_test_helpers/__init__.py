d065d86c6f517cb9800c359e3c5710e3cf77dc4c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-31 03:37:24 +0100

    project files placement refactoring

--- excel_table_cnn/train_test_helpers/__init__.py
+++ excel_table_cnn/train_test_helpers/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['dataset_loader', 'train_test_composer']
+__all__ = ['dataset_loader', 'train_test_composer', 'cell_features']
 
 from .dataset_loader import DatasetLoader
 from .markup_loader import MarkupLoader
+from .cell_features import get_table_features
