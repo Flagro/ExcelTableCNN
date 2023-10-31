56ac5ed57a5fa9ba582c12a48e8c8dfa38799cdb
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-31 02:52:32 +0100

    multiple ranges fix

--- excel_table_cnn/train_data_helpers/markup_loader.py
+++ excel_table_cnn/train_data_helpers/markup_loader.py
@@ -12,9 +12,21 @@ class MarkupLoader:
             response = requests.get(self.tablesense_url)
             data = response.content.decode('utf-8')
             
-            # Load data into a pandas DataFrame using io.StringIO
+            # Load data into a pandas DataFrame
+            df = pd.read_csv(io.StringIO(data), sep='\t', header=None)
+            
+            # Process the DataFrame to combine table_range values into a list
+            processed_data = []
+            for _, row in df.iterrows():
+                # The first 4 columns are file_name, sheet_name, set_type, and parent_path
+                file_name, sheet_name, set_type, parent_path = row[:4]
+                # All remaining columns are table_range values
+                table_ranges = row[4:].dropna().tolist()
+                processed_data.append([file_name, sheet_name, set_type, parent_path, table_ranges])
+            
+            # Convert the processed data into a DataFrame
             columns = ['file_name', 'sheet_name', 'set_type', 'parent_path', 'table_range']
-            df = pd.read_csv(io.StringIO(data), sep='\t', names=columns)
+            df = pd.DataFrame(processed_data, columns=columns)
             return df
             
         else:
