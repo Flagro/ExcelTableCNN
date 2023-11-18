483a326e4ed18a5b911a8db7c2a435330d54051a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-18 22:42:23 +0100

    updated init file

--- excel_table_cnn/train_test_helpers/__init__.py
+++ excel_table_cnn/train_test_helpers/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ['dataset_loader', 'train_test_composer', 'cell_features']
+__all__ = ['dataset_loader', 'train_test_composer', 'cell_features', 'markup_loader']
 
 from .dataset_loader import DatasetLoader
 from .markup_loader import MarkupLoader
 from .cell_features import get_table_features
+from .train_test_composer import get_train_test
