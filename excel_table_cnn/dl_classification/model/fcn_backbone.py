4c5646cfdf4c91e79d6cb00021edee42fd446a32
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-29 15:19:29 +0100

    removed feature reduction from FCN backbone

--- excel_table_cnn/dl_classification/model/fcn_backbone.py
+++ excel_table_cnn/dl_classification/model/fcn_backbone.py
@@ -1,17 +1,18 @@
 import torch.nn as nn
 import torchvision.models as models
 
-
-# Define the FCN model
 class FCNBackbone(nn.Module):
     def __init__(self, num_features):
         super().__init__()
         self.conv1 = nn.Conv2d(num_features, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, 3, kernel_size=3, padding=1)  # Reducing to 3 channels
-        self.features = models.vgg16(pretrained=True).features  # Use VGG16's features
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)  # Increasing to 256 channels
+        self.features = models.vgg16(pretrained=True).features[:-1]  # Use VGG16's features up to the second to last layer
 
     def forward(self, x):
-        x = self.conv1(x)  # Convert to 3 channels
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
         x = self.features(x)  # Apply VGG16 features
-        return x
+        # Assume x is shaped (N, 256, H', W') where H' and W' are downscaled from the original H and W.
+        return x  # The features are now ready for the RPN
