0ef20246d58f3fb90eec7b65e071a5eaa59264c1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-11-29 15:46:40 +0100

    num features for model

--- excel_table_cnn/dl_classification/model/model.py
+++ excel_table_cnn/dl_classification/model/model.py
@@ -9,7 +9,7 @@ class TableDetectionModel(torch.nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         # Initialize the backbone, a modified FCN with an appropriate number of input channels
-        self.backbone = FCNBackbone(17)
+        self.backbone = FCNBackbone(num_features=17)
 
         # Pass the backbone and the number of classes to the CustomFasterRCNN
         self.model = CustomFasterRCNN(self.backbone, num_classes)
