12a2817a32a5200b9a2b3a11e8751c1a71f88ad6
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2025-01-28 16:25:43 +0100

    Remove nan features

--- excel_table_cnn/train_test_helpers/cell_features.py
+++ excel_table_cnn/train_test_helpers/cell_features.py
@@ -6,22 +6,22 @@ def get_cell_features_xlsx(cur_cell):
     cell_features = {
         "coordinate": cur_cell.coordinate,
         "is_empty": cur_cell.value is None,
-        "is_string": cur_cell.data_type in ['s', 'str'],
-        "is_merged": type(cur_cell).__name__ == 'MergedCell',
-        "is_bold": cur_cell.font.b,
-        "is_italic": cur_cell.font.i,
+        "is_string": cur_cell.data_type in ["s", "str"],
+        "is_merged": type(cur_cell).__name__ == "MergedCell",
+        "is_bold": cur_cell.font.b or False,
+        "is_italic": cur_cell.font.i or False,
         "left_border": cur_cell.border.left is not None,
         "right_border": cur_cell.border.right is not None,
         "top_border": cur_cell.border.top is not None,
         "bottom_border": cur_cell.border.bottom is not None,
         "is_filled": cur_cell.fill.patternType is not None,
         "horizontal_alignment": cur_cell.alignment.horizontal is not None,
-        "left_horizontal_alignment": cur_cell.alignment.horizontal == 'left',
-        "right_horizontal_alignment": cur_cell.alignment.horizontal == 'right',
-        "center_horizontal_alignment": cur_cell.alignment.horizontal == 'center',
-        "wrapped_text": cur_cell.alignment.wrapText,
+        "left_horizontal_alignment": cur_cell.alignment.horizontal == "left",
+        "right_horizontal_alignment": cur_cell.alignment.horizontal == "right",
+        "center_horizontal_alignment": cur_cell.alignment.horizontal == "center",
+        "wrapped_text": cur_cell.alignment.wrapText or False,
         "indent": cur_cell.alignment.indent != 0,
-        "formula": cur_cell.data_type == 'f'
+        "formula": cur_cell.data_type == "f",
     }
     return cell_features
 
